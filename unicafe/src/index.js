ed982495b47b78a9cb6ef0758a3f0884cbdee422
Liisa Lahti <liisa.r.lahti@gmail.com>
2020-03-27 11:09:22 +0200

    1.7 Add all, average and positive statistics

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -3,24 +3,40 @@ import ReactDOM from 'react-dom';
 
 const Header = ({ text }) => <h1>{text}</h1>
 
-const Feedback = ({ text, score }) => <p>{text} {score}</p>
-
 const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Display = () => (
-  <div>
-  <Header text="give feedback" />
-  <Button text="good" handleClick={handleGood} />
-  <Button text="neutral" handleClick={handleNeutral} />
-  <Button text="bad" handleClick={handleBad} />
-  <Header text="statistics" />
-  <Feedback text="good" score={good} />
-  <Feedback text="neutral" score={neutral} />
-  <Feedback text="bad" score={bad} />
-</div>
-)
+const Statistic = ({ text, score, end }) => <p>{text} {score} {end}</p>
+
+const Feedback = ({good, bad, neutral}) => {
+  const all = good + bad + neutral
+
+  const average = () => {
+    if (all === 0) {
+      return 0
+    }
+    return (good * 1 + bad * -1) / all
+  }
+
+  const positive = () => {
+    if (all === 0) {
+      return 0
+    }
+    return 100 * good / all
+  }
+
+  return (
+    <div>
+      <Statistic text="good" score={good} />
+      <Statistic text="neutral" score={neutral} />
+      <Statistic text="bad" score={bad} />
+      <Statistic text="all" score={all} />
+      <Statistic text="average" score={average()} />
+      <Statistic text="positive" score={positive()} end="%" />
+    </div>
+  )
+}
 
 const App = () => {
 
@@ -33,7 +49,14 @@ const App = () => {
   const handleBad = () => setBad(bad + 1)
 
   return (
-
+    <div>
+      <Header text="give feedback" />
+      <Button text="good" handleClick={handleGood} />
+      <Button text="neutral" handleClick={handleNeutral} />
+      <Button text="bad" handleClick={handleBad} />
+      <Header text="statistics" />
+      <Feedback good={good} neutral={neutral} bad={bad} />
+    </div>
   )
 }
 
